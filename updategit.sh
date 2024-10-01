66a55708efdbe08031a79dac34bf30920c8aff08
azimshaikh95 <35104251+azimshaikh95@users.noreply.github.com>
2024-10-01 11:54:19 +0530

    Updated on 2024-10-01 11:54:19

--- updategit.sh
+++ updategit.sh
@@ -3,7 +3,7 @@
 git status
 git add .
 git commit -m "Updated on `date +'%Y-%m-%d %H:%M:%S'`";
-git push -u origin master
+git push -u origin main
 git status
 
-#git merge origin/master
+#git merge origin/main
